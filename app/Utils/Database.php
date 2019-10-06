3c6c095e32d89a8560fe5675ac0d90b251693e20
Tharlei <tharlei_aleixo@hotmail.com>
2019-10-06 19:51:44 -0300

    conectar heroku

--- app/Utils/Database.php
+++ app/Utils/Database.php
@@ -8,6 +8,7 @@
         public static function query($sql, $debug = false) {
             $conn = new PDO(DB_TYPE . 
                 ':host=' . DB_HOST . ';
+                port=' . DB_PORT . ';
                 dbname=' . DB_BASE, DB_USER, DB_PASS);
             // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if($debug) dd($sql);
---
+++ config.php.example
@@ -0,0 +1,9 @@
+<?php
+    define('APP_PROD', false);
+    define('DB_TYPE', 'mysql');
+    define('DB_USER', 'admin');
+    define('DB_PASS', 'admin');
+    define('DB_HOST', '127.0.0.1');
+    define('DB_BASE', 'unip');
+    define('DB_PORT', 3306);
+    define('HTTP_TYPE', "http://");
